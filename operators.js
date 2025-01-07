442780d3a9e538c347129605eaedda196f086287
varshith403 <varshithsai.nalla@gmail.com>
2025-01-07 20:07:38 +0530

    pushing the codes to repo

---
+++ operators.js
@@ -0,0 +1,16 @@
+console.log(5+5);
+console.log(10+"100");
+console.log("hi"+10+50);
+
+console.log(10-5);
+console.log("10"-5);
+
+console.log(10/5);
+console.log("10"/5);
+
+console.log(10 == 10);
+console.log("10" == 10);
+
+console.log("10" === 10);
+
+
