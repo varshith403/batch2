494110b6231a85b8afb218dd6981d24c94af13f2
varshith403 <varshithsai.nalla@gmail.com>
2025-01-08 13:18:44 +0530

    new files added

---
+++ day3/looping.js
@@ -0,0 +1,26 @@
+// for(let i=1;i<=100;i++){
+//     if(i%2==0){
+//      console.log(i + " is a even number")
+//     }else{
+//     console.log(i + " is a odd number")
+//     }
+    
+// }
+
+// // WAJP to find the factorial of a number
+// let mul=1;
+// let num = Number(prompt("enter a number"));
+// for(let i=1;i<=num;i++){
+//    mul = mul*i;
+// }
+// console.log(mul)
+
+let i =1;
+while(i <= 5){
+   console.log("hello world")
+   i++ 
+}
+
+function addtocart(){
+    alert("product ")
+}
