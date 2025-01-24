50a6aaf32ee35d84d1f1aabbe5cbcbb5af4bcef5
varshith403 <varshithsai.nalla@gmail.com>
2025-01-24 22:35:59 +0530

    push the files

--- day8/async.js
+++ day8/async.js
@@ -7,14 +7,37 @@
 // .catch(err=>console.log(err))
 // .finally(()=>console.log("finally"))
 
-function fetchusers()
-{
-    let response = fetch("https://jsonplaceholder.typicode.com/users");
-    response.then(res=>{
-        return res.json().then(data=>{
-            console.log(data);
-        })
-    })
-    .catch(err=>console.log(err))
+// api fetching
+
+// function fetchusers()
+// {
+//     let response = fetch("https://jsonplaceholder.typicode.com/users");
+//     response.then(res=>{
+//         return res.json().then(data=>{
+//             console.log(data);
+//         })
+//     })
+//     .catch(err=>console.log(err))
+// }
+// fetchusers();
+
+let p = new Promise((resolve , reject)=>{
+    setTimeout(()=>{
+        resolve("success")
+    },4000);
+});
+
+async function demo(){
+    console.log("start");
+    let x =await p;
+    console.log(x);
+    console.log("end");
 }
-fetchusers();
+demo();
+
+// async function fetchusers(){
+//     let response =await fetch("https://jsonplaceholder.typicode.com/users");
+//     let data =await response.json();
+//     console.log(data);
+// }
+// fetchusers()
---
+++ day9/dom.js
@@ -0,0 +1,23 @@
+let ele = document.getElementsByClassName("test");
+ele[0].style.backgroundColor="dodgerblue"
+//console.log(ele);
+//console.log(Array(ele));//this method used to check whether the arry is pre array.
+
+//! SPread operator:- This operator is used to take out each and every value from 
+//!Original array and store it in one more new array (pure array).
+
+//Syntax of Sread  - [...varaible]
+
+// let x = [...ele];
+// console.log(x , Array.isArray(x));
+// x.map(element=>{
+//     element.style.backgroundColor="tomato";
+//     element.style.color="white";
+//     element.style.textTransform = "uppercase";
+// })
+
+let ele1 = document.querySelector("#demo");
+console.log(ele1);
+let ele2 = document.querySelector(".test");
+console.log(ele2);
+
