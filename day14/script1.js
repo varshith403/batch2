e0d2429f36533486813f37cbed2d08a968b2da7b
varshith403 <varshithsai.nalla@gmail.com>
2025-01-30 21:16:43 +0530

    push files

---
+++ day14/script1.js
@@ -0,0 +1,4 @@
+export let msg = "hello,good morning";
+export function greet(){
+    return msg+"I am coding now"
+}
---
+++ day14/script2.js
@@ -0,0 +1,5 @@
+import {msg} from "./script1";
+import {greet}  from "./script1";
+let abc = greet();
+console.log(abc);
+console.log(greet());
